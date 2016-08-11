1c0ac58525ac05cbf5a6679ae5cb19b010109e32
Vasil <v.mitov.clouway@gmail.com>
2016-08-11 11:35:43 +0300

    Server shell

--- src/com/clouway/Server.java
+++ src/com/clouway/Server.java
@@ -1,7 +1,33 @@
 package com.clouway;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author Vasil Mitov <v.mitov.clouway@gmail.com>
  */
-public class Server {
+public class Server implements Runnable {
+  private Integer port;
+  private String message;
+
+  public Server(Integer port, String message) {
+    this.port = port;
+    this.message = message;
+  }
+
+  @Override
+  public void run() {
+    try {
+      ServerSocket serverSocket = new ServerSocket(port);
+      while (true) {
+        Socket clientSocket = serverSocket.accept();
+        PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+        output.println(message );
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
