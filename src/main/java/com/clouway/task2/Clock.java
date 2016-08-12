f80ff2f34d8f862e5756da6099dd75bdf808a10d
Borislav Gadjev <gadjevb@gmail.com>
2016-08-12 10:57:31 +0300

    Fix!

--- src/main/java/com/clouway/task2/Clock.java
+++ src/main/java/com/clouway/task2/Clock.java
@@ -6,9 +6,5 @@ import java.util.Date;
  * @author Borislav Gadjev <gadjevb@gmail.com>
  */
 public interface Clock {
-
-    /**
-     * @return current time and date
-     */
     Date getTimeAndDate();
 }
--- src/main/java/com/clouway/task3/Screen.java
+++ src/main/java/com/clouway/task3/Screen.java
@@ -4,6 +4,5 @@ package com.clouway.task3;
  * @author Borislav Gadjev <gadjevb@gmail.com>
  */
 public interface Screen {
-    // method display, displays the received String message
     void display(String message) throws NoSocketException;
 }
