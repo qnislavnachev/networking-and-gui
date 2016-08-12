e570cc335b88699290e1118343587d93fac9b6e1
Borislav Gadjev <gadjevb@gmail.com>
2016-08-12 10:58:43 +0300

    Removed the unnecessary documentation from ProgressBar

--- src/main/java/com/clouway/task1/ProgressBar.java
+++ src/main/java/com/clouway/task1/ProgressBar.java
@@ -3,12 +3,6 @@ package com.clouway.task1;
 /**
  * @author Borislav Gadjev <gadjevb@gmail.com>
  */
-
-/**
- * ProgressBar.java
- * Interface class that has the following methods
- */
 public interface ProgressBar {
-    // method that returns an integer corresponding to the current download progress
     int getProgress();
 }
